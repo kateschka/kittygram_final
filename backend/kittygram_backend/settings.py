6059523aa0816301b16f144613aaa4c5461afb84
Ekaterina Kuznetsova <sova.krs@gmail.com>
2025-02-17 20:04:23 +0300

    Edit ALLOWED_HOSTS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 'kattygram.duckdns.org', '158.160.75.150']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
